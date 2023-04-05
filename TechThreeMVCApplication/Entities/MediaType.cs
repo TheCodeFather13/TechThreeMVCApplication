3ea8392f54a7972af6e97fa65f77abf8a1a21590
Michael <98636660+TheCodeFather13@users.noreply.github.com>
2023-04-05 18:47:25 +0300

    Added CRUD for CategoryItems

--- TechThreeMVCApplication/Entities/MediaType.cs
+++ TechThreeMVCApplication/Entities/MediaType.cs
@@ -4,10 +4,11 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using TechThreeMVCApplication.Interfaces;
 
 namespace TechThreeMVCApplication.Entities
 {
-    public class MediaType
+    public class MediaType : IPrimaryProperties
     {
         public int Id { get; set; }
 
---
+++ TechThreeMVCApplication/Interfaces/IPrimaryProperties.cs
@@ -0,0 +1,8 @@
+﻿namespace TechThreeMVCApplication.Interfaces
+{
+    public interface IPrimaryProperties
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+    }
+}
